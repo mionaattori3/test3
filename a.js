8a190583e55dc72f2fe0b3b9d523a68f5844ba52
mionaattori <mionaattori2@gmail.com>
2026-03-03 09:24:17 +0200

    testing

---
+++ a.js
@@ -0,0 +1,21 @@
+
+
+function doSomething() {
+    console.log("Doing something...");
+    const B = document.getElementById("nappi")
+    const V = document.getElementById("viesti")
+    const P = document.getElementById("password")
+    B.addEventListener("click",()=>{
+        if (B.textContent === "piilota"){
+        V.style.display = "none";
+        B.textContent = "näytä"
+        }
+        else if (B.textContent === "näytä" && P.value === "LeoIsStupid"){
+            V.style.display = "flex";
+            B.textContent = "piilota"
+        }
+    })
+    
+}
+
+doSomething()
